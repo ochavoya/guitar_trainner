a20d005a9ae6f6507243da154e42d2a42d1e1466
ochavoya <31660622+ochavoya@users.noreply.github.com>
2018-12-24 07:20:54 -0700

    Updated the Bass.java file to improve readability

--- src/com/ochavoya/midi/guitar/Bass.java
+++ src/com/ochavoya/midi/guitar/Bass.java
@@ -5,12 +5,14 @@ import com.ochavoya.midi.musictheory.MidiNote;
 
 public class Bass extends Guitar{
 	
+	private static final int FRETS = 19;
+	
 	/*
-	 * The bass is supposed to have four strings. It starts with MidiNote
+	 * The bass is supposed to have four strings. It starts with MidiNote(0,28) [E A D F]
 	 */
 
 	public Bass(){
-		super(new MidiNote[]{new MidiNote(0,28),new MidiNote(0,33), new MidiNote(0,38),new MidiNote(0,43)},19);
+		super(new MidiNote[]{new MidiNote(0,28),new MidiNote(0,33), new MidiNote(0,38),new MidiNote(0,43)}, FRETS);
 		name="Bass";
 	}
 
